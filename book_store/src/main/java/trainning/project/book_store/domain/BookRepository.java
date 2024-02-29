b1959789c04912bb7a7c7891f172968d559d3f85
Vo Minh Thien NGUYEN <minhthien.ng4612@gmail.com>
2024-02-29 09:09:53 +0200

    changes

--- book_store/src/main/java/trainning/project/book_store/domain/BookRepository.java
+++ book_store/src/main/java/trainning/project/book_store/domain/BookRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-
-
 public interface BookRepository extends CrudRepository<Book, Long> {
     List<Book> findByTitle(String title);
 }
